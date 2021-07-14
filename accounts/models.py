15ebae62cd693dd635e2cb8c180729db554c37f7
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-07-15 00:26:05 +0300

    fixes3

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     def __str__(self):

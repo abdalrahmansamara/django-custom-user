81c1cc8c9e2230839bf21c6d7e2ce9cccb520376
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-07-15 00:05:25 +0300

    fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email',]
+    list_display = ['email', 'username',]
 
 admin.site.register(CustomUser, CustomUserAdmin)
